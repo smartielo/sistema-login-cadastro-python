0e55238f33f2abb59d95244e21e73c94ddb2cbd9
Gabriel Martielo <gabrielmartielo3214@gmail.com>
2025-03-26 16:41:37 -0300

    criação das label, entry e button

--- main.py
+++ main.py
@@ -26,3 +26,27 @@ app.geometry('300x300')
 label_usuario = ctk.CTkLabel(app, text='Usuário') #cria o label
 label_usuario.pack(pady=10) #coloca o código da label na app
 
+#entry
+campo_usuario = ctk.CTkEntry(app, placeholder_text='Digite seu usuário') #cria o campo de entrada
+campo_usuario.pack(pady=5) #coloca o código na app
+
+#label
+label_senha = ctk.CTkLabel(app, text='Senha') #cria o label
+label_senha.pack(pady=10) #coloca o código da label na app
+
+#entry
+campo_senha = ctk.CTkEntry(app, placeholder_text='Digite sua senha') #cria o campo de entrada
+campo_senha.pack(pady=5) #coloca o código na app
+
+#button
+ctk.CTkButton(app, text='Login', command=validar_login).pack(pady=10) #cria o botão e coloca o código na app
+
+#campo de texto feedback do login
+resultado_login = ctk.CTkLabel(app, text='')
+resultado_login.pack(pady=10)
+
+
+#inicia a janela
+app.mainloop()
+
+
